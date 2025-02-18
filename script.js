6dc241f806979d81ce1e5c49e6bf32d8aa733d42
Dhruv <dhruv.p7115@gmail.com>
2025-02-18 12:40:15 -0500

    Implemented the functionality to show markups on maps

--- script.js
+++ script.js
@@ -9,8 +9,41 @@ async function initMap() {
     center: { lat: 43.2557, lng: -79.8711 },
     zoom: 8,
   });
+
+  addMarkers();
 }
 
 initMap();
+
+const locations = [
+  { name: "Gage Park", lat: 43.2413, lng: -79.8305, category: "parks" },
+  { name: "Dundurn Castle", lat: 43.2733, lng: -79.8840, category: "museums" },
+  { name: "Art Gallery of Hamilton", lat: 43.2579, lng: -79.8736, category: "museums" },
+];
+
+function addMarkers() {
+  locations.forEach((location) => {
+      const marker = new google.maps.Marker({
+          position: { lat: location.lat, lng: location.lng },
+          map,
+          title: location.name,
+          category: location.category,
+      });
+
+      const infoWindow = new google.maps.InfoWindow({
+          content: `<h3>${location.name}</h3>`,
+      });
+
+      marker.addListener("click", () => {
+          infoWindow.open(map, marker);
+      });
+
+      markers.push(marker);
+  });
+}
+
+
+
+
 
 
